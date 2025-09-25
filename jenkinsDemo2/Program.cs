de92462ed61d02fee8bdde09d54226405fdaa7cf
aditya070901 <adityalatkar65@gmail.com>
2025-09-25 17:08:45 +0530

    Add project files.

---
+++ jenkinsDemo2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jenkinsDemo2
+{
+    internal class Program
+    {
+        public int Add(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+
+        public int Sub(int a, int b)
+        {
+            int c = a - b;
+            return c;
+        }
+
+        public int Multi(int a, int b)
+        {
+            int c = a * b;
+            return c;
+        }
+
+        public int Div(int a, int b)
+        {
+            int c = a / b;
+            return c;
+        }
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Add(1, 2);
+            p.Sub(3, 4);
+            p.Multi(5,6);
+            p.Div(5, 6);
+        }
+    }
+}
